44dbe2b42e8b2cb2a36afeec062c4192ea08171a
Lupeman <jaimegunther@Jaimes-MacBook-Air.local>
2016-12-11 22:32:14 +1100

    modify css, enabled toggle to hide movie information until needed

--- Jaime/WEEK4/d5/app.rb
+++ Jaime/WEEK4/d5/app.rb
@@ -4,13 +4,21 @@ require "sinatra/reloader"
 require "httparty"
 
 get "/" do
-  "Hello World"
-  # @movie = params[:movie]
-  # @result = HTTParty.get('http://omdbapi.com/?t=@movie')
-  # @movie_info =
+  @movie = params[:movie]
+  @movie_info = HTTParty.get("http://omdbapi.com/?t=#{@movie}")
+  @movie_title = @movie_info["Title"]
+  @movie_year = @movie_info["Year"]
+@movie_rated = @movie_info["Rated"]
+@movie_released = @movie_info["Released"]
+@movie_runtime = @movie_info["Runtime"]
+@movie_genre = @movie_info["Genre"]
+@movie_director = @movie_info["Director"]
+@movie_writer = @movie_info["Writer"]
+@movie_actors = @movie_info["Actors"]
+@movie_plot = @movie_info["Plot"]
   erb :index
 end
 
-
+#
 #
 # binding.pry
