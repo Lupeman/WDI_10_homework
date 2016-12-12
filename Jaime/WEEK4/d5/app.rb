74fa45542df72ac85ef228f268679d77d47ec3c1
Lupeman <jaimegunther@Jaimes-MacBook-Air.local>
2016-12-12 17:21:28 +1100

    add movie_info page, modify app to search for multiple titles

--- Jaime/WEEK4/d5/app.rb
+++ Jaime/WEEK4/d5/app.rb
@@ -4,20 +4,30 @@ require "sinatra/reloader"
 require "httparty"
 
 get "/" do
-  @movie = params[:movie]
-  @movie_info = HTTParty.get("http://omdbapi.com/?t=#{@movie}")
-  @movie_title = @movie_info["Title"]
-  @movie_year = @movie_info["Year"]
-@movie_rated = @movie_info["Rated"]
-@movie_released = @movie_info["Released"]
-@movie_runtime = @movie_info["Runtime"]
-@movie_genre = @movie_info["Genre"]
-@movie_director = @movie_info["Director"]
-@movie_writer = @movie_info["Writer"]
-@movie_actors = @movie_info["Actors"]
-@movie_plot = @movie_info["Plot"]
   erb :index
 end
+
+get "/movie" do
+  @movie = params[:title]
+  @movie_info = HTTParty.get("http://omdbapi.com/?s=#{@movie}")
+    # binding.pry
+  # @movie_title = @movie_info["Search"]
+  # @movie_year = @movie_info["Year"]
+  # @movie_rated = @movie_info["Rated"]
+  # @movie_released = @movie_info["Released"]
+  # @movie_runtime = @movie_info["Runtime"]
+  # @movie_genre = @movie_info["Genre"]
+  # @movie_director = @movie_info["Director"]
+  # @movie_writer = @movie_info["Writer"]
+  # @movie_actors = @movie_info["Actors"]
+  # @movie_plot = @movie_info["Plot"]
+  erb :movie_info
+end
+
+
+
+
+
 
 #
 #
