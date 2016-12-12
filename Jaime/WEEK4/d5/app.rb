1599ea267ebdce5e80cd4a6925f867a71d68b19f
Lupeman <jaimegunther@Jaimes-MacBook-Air.local>
2016-12-12 21:59:36 +1100

    update search function - click on movie from list of titles to bring to details page

--- Jaime/WEEK4/d5/app.rb
+++ Jaime/WEEK4/d5/app.rb
@@ -7,23 +7,28 @@ get "/" do
   erb :index
 end
 
-get "/movie" do
+get "/movie_list" do
   @movie = params[:title]
   @movie_info = HTTParty.get("http://omdbapi.com/?s=#{@movie}")
-    # binding.pry
-  # @movie_title = @movie_info["Search"]
-  # @movie_year = @movie_info["Year"]
-  # @movie_rated = @movie_info["Rated"]
-  # @movie_released = @movie_info["Released"]
-  # @movie_runtime = @movie_info["Runtime"]
-  # @movie_genre = @movie_info["Genre"]
-  # @movie_director = @movie_info["Director"]
-  # @movie_writer = @movie_info["Writer"]
-  # @movie_actors = @movie_info["Actors"]
-  # @movie_plot = @movie_info["Plot"]
-  erb :movie_info
+  erb :movie_list
 end
 
+get "/movie" do
+  @movie_name = params[:param1]
+  @movie_info = HTTParty.get("http://omdbapi.com/?t=#{@movie_name}")
+   @movie_title = @movie_info["Title"]
+   @movie_year = @movie_info["Year"]
+   @movie_rated = @movie_info["Rated"]
+   @movie_released = @movie_info["Released"]
+   @movie_runtime = @movie_info["Runtime"]
+   @movie_genre = @movie_info["Genre"]
+   @movie_director = @movie_info["Director"]
+   @movie_writer = @movie_info["Writer"]
+   @movie_actors = @movie_info["Actors"]
+   @movie_plot = @movie_info["Plot"]
+   @movie_poster = @movie_info["Poster"]
+  erb :movie
+end
 
 
 
